02966d4d4d999553433639ab273f58ef19dfd8d1
Daniel <jdb12@rice.edu>
2018-06-08 07:57:07 -0500

    testing background

--- modules/BackgroundMaker.h
+++ modules/BackgroundMaker.h
@@ -112,19 +112,25 @@ public:
 		map<string,TH1 * > h1d;
 
 		RooPlotLib rpl;
+		float minPt = config.get<float>("p.minPt");
+
 		vector<string> components = { "eta_mumu", "ccbar_mumu", "jpsi_mumu", "omega_mumu", "omega_pi0mumu", "rho_mumu", "phi_mumu", "eta_gammamumu", "etaprime_gammamumu" };
 		for ( string n : components ){
 			h2d[ n ] = get<TH2>( "Scaled_pT_" + n );
 			LOG_F( INFO, "Cocktail: %s", n.c_str()  );
-			h1d[ n ] = h2d[ n ]->ProjectionX( (n + "_mass"/*, ipt1, ipt2*/).c_str() );
+
+
+			int ipt1 = h2d[n]->GetYaxis()->FindBin( minPt );
+			LOG_F( INFO, "minPt = %f, bin = %d", minPt, ipt1 );
+			h1d[ n ] = h2d[ n ]->ProjectionX( (n + "_mass").c_str(), ipt1, -1 );
 			if ( n == "eta_mumu" ){
 				h1d[ "sum" ] = (TH1*)h1d[ n ]->Clone( "cocktail_sum" );
 				h1d[ "sum" ]->Reset();
 			}
 			if ( n != "ccbar_mumu" ){
-				h1d[ n ]->Scale( 3.5e11 * config.get<float>( "p.eff", 1.0 ), "width" );
+				h1d[ n ]->Scale( 3.56e11 * config.get<float>( "p.eff", 1.0 ), "width" );
 			} else {
-				h1d[ n ]->Scale( 3.5e11 * config.get<float>( "p.eff", 1.0 ) );
+				h1d[ n ]->Scale( 3.56e11 * config.get<float>( "p.eff", 1.0 ) );
 			}
 			h1d[ "sum" ]->Add( h1d[ n ] );
 
@@ -182,8 +188,13 @@ public:
 			hraw_bg->SetBinContent( i, v * cf );
 		}
 
+		
+
 		TH1 * hrb_bg = HistoBins::rebin1D( "rb_bg", hraw_bg, bins["mass"] );
 		hrb_bg->Scale( 1.0, "width" );
+		
+		hraw_bg->Scale( config.get<float>("p.bgscaler", 1.0) );
+		hrb_bg->Scale( config.get<float>("p.bgscaler", 1.0) );
 
 		Reporter rp( "rpBackground-" + config["sys"] + "-" + config["mod"] + config["debug"] + ".pdf", 1900, 1200 );
 		rp.newPage();
@@ -191,11 +202,16 @@ public:
 
 		RooPlotLib rpl;
 		rpl.link( book );
+		rpl.link( &config );
 
 
 		hraw_ls->Scale( 1.0, "width" );
 		hraw_mix->Scale( 1.0, "width" );
 
+		/*************************************************************************************************************************
+		* Page 1
+		* Like-Sign vs. Mixed Like-Sign 
+		* ******************************/
 		rpl.style( hraw_ls ).set( config, "style.ls" ).draw();
 		rpl.style( hrb_ls ).set( config, "style.ls" ).draw("same");
 		rpl.style( hraw_mix ).set( config, "style.mixed" ).draw("same");
@@ -203,15 +219,20 @@ public:
 
 		rp.next();
 
+		/*************************************************************************************************************************
+		* Page 2
+		* Raw binning correction factor
+		* ******************************/
 		rpl.style( hraw_corr ).set( config, "style.corr" ).draw();
-
 		rp.next();
 
+		/*************************************************************************************************************************
+		* Page 3
+		* Rebinned correction factor
+		* ******************************/
 		rpl.style( hrb_corr ).set( config, "style.corr" ).draw();
-
 		rp.next();
 
-		// hraw_mix->Scale(1.0, "width" );
 		hraw_bg->Scale(1.0, "width" );
 
 		vector<float> legPos = config.getFloatVector( "TLegend[0]:pos" );
@@ -223,7 +244,10 @@ public:
 		leg0->AddEntry( hraw_bg, "Corr BG", "lp" );
 
 
-
+		/*************************************************************************************************************************
+		* Page 4
+		* Mixed + correction factor applied BG
+		* ******************************/
 
 		rpl.style( hraw_bg ).set( config, "style.bg" ).draw();
 		rpl.style( hrb_bg ).set( config, "style.bg" ).draw("same");
@@ -240,6 +264,11 @@ public:
 		rp.next();
 
 		hraw_uls->Scale( 1.0, "width" );
+
+		/*************************************************************************************************************************
+		* Page 5
+		* Unlike-Sign + Background
+		* ******************************/
 
 		rpl.style( hraw_uls ).set( config, "style.uls" ).draw();
 		rpl.style( hrb_uls ).set( config, "style.uls" ).draw("same");
@@ -260,6 +289,10 @@ public:
 
 		rp.next();
 
+		/*************************************************************************************************************************
+		* Page 6
+		* Unlike-Sign + Background semilogy
+		* ******************************/
 
 		rpl.style( hraw_uls ).set( config, "style.uls" ).draw();
 		rpl.style( hrb_uls ).set( config, "style.uls" ).draw("same");
@@ -277,12 +310,18 @@ public:
 
 		rp.next();
 
+
 		TH1 * hraw_sig = (TH1*)hraw_uls->Clone( "hraw_sig" );
 		TH1 * hrb_sig  = (TH1*)hrb_uls->Clone( "hrb_sig" );
 
 		hraw_sig->Add( hraw_bg, -1 );
 		hrb_sig->Add( hrb_bg, -1 );
 
+
+		/*************************************************************************************************************************
+		* Page 7
+		* Signal linear y
+		* ******************************/
 		rpl.style( hraw_uls ).set( config, "style.uls" ).draw();
 		rpl.style( hrb_uls ).set( config, "style.uls" ).draw("same");
 		rpl.style( hraw_bg ).set( config, "style.bg" ).draw("same");
@@ -302,6 +341,11 @@ public:
 
 		rp.next();
 
+
+		/*************************************************************************************************************************
+		* Page 8
+		* Signal semi-log y
+		* ******************************/
 		rpl.style( hraw_uls ).set( config, "style.uls" ).set( "min", 1 ).draw();
 		rpl.style( hrb_uls ).set( config, "style.uls" ).draw("same");
 		rpl.style( hraw_bg ).set( config, "style.bg" ).draw("same");
@@ -321,8 +365,41 @@ public:
 		rp.next();
 
 
-		// Draw with the Cocktail
-		// 
+		/*************************************************************************************************************************
+		* Page 9
+		* Signal / Background
+		* ******************************/
+
+		TH1 * hraw_SoverB = (TH1*)hraw_sig->Clone( "raw_SoverB" );
+		TH1 * hrb_SoverB  = (TH1*)hrb_sig->Clone( "rb_SoverB" );
+
+		hraw_SoverB->Divide( hraw_bg );
+		hrb_SoverB->Divide( hrb_bg );
+
+		rpl.style( hrb_SoverB ).set( "style.SoverB" ).draw();
+		// rpl.style( hraw_uls ).set( config, "style.uls" ).set( "min", 1 ).draw();
+		// rpl.style( hrb_uls ).set( config, "style.uls" ).draw("same");
+		// rpl.style( hraw_bg ).set( config, "style.bg" ).draw("same");
+		// rpl.style( hrb_bg ).set( config, "style.bg" ).draw("same");
+		// rpl.style( hraw_mix ).set( config, "style.mixed" ).draw("same");
+		// rpl.style( hrb_mix ).set( config, "style.mixed" ).draw("same");
+		// // rpl.style( hraw_sig ).set( config, "style.sig" ).draw("same");
+		// rpl.style( hrb_sig ).set( config, "style.sig" ).draw("same");
+		// gPad->SetLogy(1);
+
+		// leg0->Draw("same");
+
+		lx.DrawLatexNDC( 0.6, 0.9, TString::Format( "Run15 %s at #sqrt{s} = 200 GeV", config["sys"].c_str() ) );
+		lx.DrawLatexNDC( 0.2, 0.90, "p_{T}^{#mu} > 1.1 (GeV/c), |#eta^{#mu}| < 0.5" );
+		lx.DrawLatexNDC( 0.2, 0.85, "p_{T}^{#mu#mu} > 0.0 (GeV/c), |y_{#mu#mu}| < 0.5" );
+
+		rp.next();
+
+
+		/*************************************************************************************************************************
+		* Page 10
+		* Signal + Cocktail
+		* ******************************/
 		rpl.style( hraw_uls ).set( config, "style.uls" ).set( "min", 10 ).draw();
 		rpl.style( hrb_uls ).set( config, "style.uls" ).draw("same");
 		rpl.style( hraw_bg ).set( config, "style.bg" ).draw("same");
@@ -342,6 +419,7 @@ public:
 		lx.DrawLatexNDC( 0.2, 0.90, "p_{T}^{#mu} > 1.1 (GeV/c), |#eta^{#mu}| < 0.5" );
 		lx.DrawLatexNDC( 0.2, 0.85, "p_{T}^{#mu#mu} > 0.0 (GeV/c), |y_{#mu#mu}| < 0.5" );
 		rp.next();
+
 
 
 	}
